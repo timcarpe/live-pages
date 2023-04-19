9c4d720878d1af606f8dc8c895548a00f5ffa9fa
timcarpe <tim.carpe@gmail.com>
2023-04-19 21:40:13 +0700

    Header images
    Header images

--- javascripts/header.js
+++ javascripts/header.js
@@ -1,8 +1,14 @@
 document.write('\
 <header>\
-<img class="headerimage" src="headerimg.jpeg">\
+<img class="headerimage" src="bwheaderbg.png">\
 <h1 class="booktitle">Timothy R. Carpenter</h1>\
+<img class="popupauthor" src="mecrop.jpg">\
 <p>Online Resume and Portfolio</p>\
-<p><small>Educator | Programmer | Volunteer | Explorer</small></p>\
+<ul>\
+<li><p>Educator</p></li>\
+<li><p>Programmer</p></li>\
+<li><p>Volunteer</p></li>\
+<li><p>Explorer</p></li>\
+</ul>\
 </header>\
 ');
